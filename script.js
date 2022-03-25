44ab2e2379d99acb1d1cb13968e52b654892e08a
ejsmart111 <egbeejay@gmail.com>
2022-03-25 15:47:14 +0100

    change variable names and addition of instructions

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ const leftWheel = document.getElementById('left-wheel')
 const gameOver = document.getElementById('game-over')
 const score = document.getElementById('score')
 const highScore = document.getElementById('hiscore')
+const instruction = document.getElementById('instruction')
+
 const hiScore = window.localStorage.getItem('hiScore')?window.localStorage.getItem('hiScore'):0
-const startGame = new Audio('./assets/start-game.wav');
-const jumpPlay = new Audio('./assets/jump.wav');
-const levelUp = new Audio('./assets/next-level.wav');
-const end = new Audio('./assets/game-over.wav');
+let isDead = false
+let scoreVal = 0
+
+const jumpPlayAudio = new Audio('./assets/jump.wav');
+const levelUpAudio = new Audio('./assets/next-level.wav');
+const endAudio = new Audio('./assets/game-over.wav');
+
 highScore.innerHTML = '<h3>'+ hiScore +'</h3>'
 
 function start() {
@@ -30,11 +35,11 @@ function start() {
     }
 
     document.addEventListener('keydown', e => {
-        jumpPlay.play()
-        jumpFunction(e) 
+        if (!isDead) {
+            jumpPlayAudio.play()
+            jumpFunction(e) 
+        }
     })
-
-    let scoreVal = 0
 
     const dead = setInterval(() => {
         if ( scoreVal <= 1000 && scoreVal >= 0) {
@@ -45,19 +50,25 @@ function start() {
             obstacles.style.animation = 'block-animation 1.4s infinite linear'
         } else if (scoreVal <= 4500 && scoreVal > 3500)  {
             obstacles.style.animation = 'block-animation 1.0s infinite linear'
-        } else {
+        } else if(scoreVal <= 7000 && scoreVal > 4500) {
+            obstacles.style.animation = 'block-animation 0.9s infinite linear'
+        } else if(scoreVal <= 13500 && scoreVal > 7000) {
             obstacles.style.animation = 'block-animation 0.8s infinite linear'
+        } else {
+            obstacles.style.animation = 'block-animation 0.7s infinite linear'
         }
         const manTop = parseInt(window.getComputedStyle(man).getPropertyValue('bottom'))
         const obsLeft = parseInt(window.getComputedStyle(obstacles).getPropertyValue('left'))
         score.innerHTML = '<h3>'+ scoreVal++ +'</h3>'
         if (obsLeft < 330 && obsLeft > 0 && manTop <= 40) {
-            end.play()
+            gameOver.style.display = 'block'
+            endAudio.play()
+            isDead = true
+            instruction.style.display = 'block'
             obstacles.style.animation = 'none'
             obstacles.style.left = 300 + 'px'
             rightWheel.classList.remove('wheel-animation')
             leftWheel.classList.remove('wheel-animation')
-            gameOver.style.display = 'block'
             if (scoreVal > parseInt(hiScore)) {
                 highScore.innerHTML = '<h3>'+ scoreVal +'</h3>'
                 window.localStorage.setItem('hiScore', scoreVal)
@@ -68,15 +79,19 @@ function start() {
 }
 
 document.addEventListener('keydown', e => {
-    gameOver.style.display = 'none'
     if (e.code === 'Enter') {
+        obstacles.style.left =  68 + 'em'
+        gameOver.style.display = 'none'
+        instruction.style.display = 'none'
+        isDead = false
         const st = setInterval(() => {
-            levelUp.play()
+            levelUpAudio.play()
         }, 100)
         setTimeout(() => {
             start()
-            startGame.pause()
             clearInterval(st)
-        }, 4000);
+        }, 6000);
     }
+
+
 })
